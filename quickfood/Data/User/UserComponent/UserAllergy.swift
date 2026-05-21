800c0ab54cdce21ab5c932904fad0c09eb7e6b98
johanpramudito <ajohanpramudito@gmail.com>
2026-05-21 22:49:03 +0700

    fix and feat: change UserAllergy and add title and icon

--- quickfood/Data/User/UserComponent/UserAllergy.swift
+++ quickfood/Data/User/UserComponent/UserAllergy.swift
@@ -5,7 +5,7 @@
 //  Created by Bintang Marsyuma Rakhasunu on 20/05/26.
 //
 
-enum UserAllergy: String, Codable {
+enum UserAllergy: String, Codable, Hashable, CaseIterable {
     case peanuts
     case treeNuts
     case seafood
@@ -16,5 +16,38 @@ enum UserAllergy: String, Codable {
     case wheat
     case sesame
     case gluten
-    case other
+    case offal
+
+    var title: String {
+        switch self {
+        case .peanuts: "Peanuts"
+        case .treeNuts: "Tree Nuts"
+        case .seafood: "Seafood"
+        case .shellfish: "Shellfish"
+        case .egg: "Egg"
+        case .milk: "Milk"
+        case .soy: "Soy"
+        case .wheat: "Wheat"
+        case .sesame: "Sesame"
+        case .gluten: "Gluten"
+        case .offal: "Offal"
+        }
+    }
+
+    var icon: String {
+        switch self {
+        case .peanuts: "🥜"
+        case .treeNuts: "🌰"
+        case .seafood: "🐟"
+        case .shellfish: "🦐"
+        case .egg: "🥚"
+        case .milk: "🥛"
+        case .soy: "🫘"
+        case .wheat: "🌾"
+        case .sesame: "⚪"
+        case .gluten: "🍞"
+        case .offal: "🧠"
+        }
+    }
 }
+
